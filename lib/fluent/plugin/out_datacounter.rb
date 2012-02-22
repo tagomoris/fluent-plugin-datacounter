422fe90ab4de2da0a39746fd06f32c390e50db81
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-02-22 17:20:45 +0900

    write implementation and its tests

--- lib/fluent/plugin/out_datacounter.rb
+++ lib/fluent/plugin/out_datacounter.rb
@@ -1,39 +1,184 @@
 require "fluent/plugin/out_datacounter/version"
 
-class Fluent::DataCounterOutput < Fluent::BufferedOutput
+class Fluent::DataCounterOutput < Fluent::Output
   Fluent::Plugin.register_output('datacounter', self)
 
-  # config_param :hoge, :string, :default => 'hoge'
+  config_param :unit, :string, :default => 'minute'
+  config_param :aggregate, :string, :default => 'tag'
+  config_param :tag, :string, :default => 'datacount'
+  config_param :input_tag_remove_prefix, :string, :default => nil
+  config_param :count_key, :string
 
-  def initialize
-    super
-    # require 'hogepos'
+  # pattern0 reserved as unmatched counts
+  config_param :pattern1, :string # string: NAME REGEXP
+  (2..9).each do |i|
+    config_param ('pattern' + i.to_s).to_sym, :string, :default => nil # NAME REGEXP
   end
+
+  attr_accessor :counts
+  attr_accessor :last_checked
 
   def configure(conf)
     super
-    # @path = conf['path']
+
+    @unit = case @unit
+            when 'minute' then :minute
+            when 'hour' then :hour
+            when 'day' then :day
+            else
+              raise Fluent::ConfigError, "flowcounter unit allows minute/hour/day"
+            end
+    @aggregate = case @aggregate
+                 when 'tag' then :tag
+                 when 'all' then :all
+                 else
+                   raise Fluent::ConfigError, "flowcounter aggregate allows tag/all"
+                 end
+
+    @patterns = [[0, 'unmatched', nil]]
+    pattern_names = ['unmatched']
+    (1..9).each do |i|
+      next unless conf["pattern#{i}"]
+      name,regexp = conf["pattern#{i}"].split(' ', 2)
+      @patterns.push([i, name, Regexp.new(regexp)])
+      pattern_names.push(name)
+    end
+    pattern_index_list = conf.keys.select{|s| s =~ /^pattern\d$/}.map{|v| (/^pattern(\d)$/.match(v))[1].to_i}
+    unless pattern_index_list.reduce(true){|v,i| v and @patterns[i]}
+      raise Fluent::ConfigError, "jump of pattern index found"
+    end
+    unless @patterns.length == pattern_names.uniq.length
+      raise Fluent::ConfigError, "duplicated pattern names"
+    end
+
+    if @input_tag_remove_prefix
+      @removed_prefix_string = @input_tag_remove_prefix + '.'
+      @removed_length = @removed_prefix_string.length
+    end
+
+    @counts = count_initialized
   end
 
   def start
     super
-    # init
+    @counts = count_initialized
+    start_watch
   end
 
   def shutdown
     super
-    # destroy
+    @watcher.terminate
+    @watcher.join
   end
 
-  def format(tag, time, record)
-    [tag, time, record].to_msgpack
+  def count_initialized(keys=nil)
+    # counts['tag'][num] = count
+    if @aggregate == :all
+      {'all' => ([0] * @patterns.length)}
+    elsif keys
+      Hash[[keys, ([[0] * @patterns.length])*(keys.length)].transpose]
+    else
+      {}
+    end
   end
 
-  def write(chunk)
-    records = []
-    chunk.msgpack_each { |record|
-      # records << record
-    }
-    # write records
+  def countups(tag, counts)
+    if @aggregate == :all
+      tag = 'all'
+    end
+    @counts[tag] ||= [0] * @patterns.length
+
+    counts.each_with_index do |count, i|
+      @counts[tag][i] += count
+    end
+  end
+
+  def stripped_tag(tag)
+    return tag unless @input_tag_remove_prefix
+    return tag[@removed_length..-1] if tag.start_with?(@removed_prefix_string) and tag.length > @removed_length
+    return tag[@removed_length..-1] if tag == @input_tag_remove_prefix
+    tag
+  end
+
+  def generate_output(counts, step)
+    output = {}
+    if @aggregate == :all
+      sum = counts['all'].inject(:+)
+      counts['all'].each_with_index do |count,i|
+        name = @patterns[i][1]
+        output[name + '_count'] = count
+        output[name + '_rate'] = ((count * 100.0) / (1.00 * step)).floor / 100.0
+        output[name + '_percentage'] = count * 100.0 / (1.00 * sum)
+      end
+      return output
+    end
+
+    counts.keys.each do |tag|
+      t = stripped_tag(tag)
+      sum = counts[tag].inject(:+)
+      counts[tag].each_with_index do |count,i|
+        name = @patterns[i][1]
+        output[t + '_' + name + '_count'] = count
+        output[t + '_' + name + '_rate'] = ((count * 100.0) / (1.00 * step)).floor / 100.0
+        output[t + '_' + name + '_percentage'] = count * 100.0 / (1.00 * sum)
+      end
+    end
+    output
+  end
+
+  def flush(step)
+    flushed,@counts = @counts,count_initialized(@counts.keys)
+    generate_output(flushed, step)
+  end
+
+  def flush_emit(step)
+    Fluent::Engine.emit(@tag, Fluent::Engine.now, flush(step))
+  end
+
+  def start_watch
+    # for internal, or tests only
+    @watcher = Thread.new(&method(:watch))
+  end
+  
+  def watch
+    # instance variable, and public accessable, for test
+    @last_checked = Fluent::Engine.now
+    tick = case @unit
+           when :minute then 60
+           when :hour then 3600
+           when :day then 86400
+           else 
+             raise RuntimeError, "@unit must be one of minute/hour/day"
+           end
+    while true
+      sleep 0.5
+      if Fluent::Engine.now - @last_checked >= tick
+        now = Fluent::Engine.now
+        flush_emit(now - @last_checked)
+        @last_checked = now
+      end
+    end
+  end
+
+  def emit(tag, es, chain)
+    c = [0] * @patterns.length
+
+    es.each do |time,record|
+      value = record[@count_key]
+      next if value.nil?
+
+      value = value.to_s
+      matched = false
+      @patterns.each do |index, name, regexp|
+        next unless regexp and regexp.match(value)
+        c[index] += 1
+        matched = true
+        break
+      end
+      c[0] += 1 unless matched
+    end
+    countups(tag, c)
+
+    chain.next
   end
 end
--- test/plugin/test_out_datacounter.rb
+++ test/plugin/test_out_datacounter.rb
@@ -6,51 +6,200 @@ class DataCounterOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+    unit minute
+    aggregate tag
+    input_tag_remove_prefix test
+    count_key target
+    pattern1 status2xx ^2\\d\\d$
+    pattern2 status3xx ^3\\d\\d$
+    pattern3 status4xx ^4\\d\\d$
+    pattern4 status5xx ^5\\d\\d$
   ]
-  # CONFIG = %[
-  #   path #{TMP_DIR}/out_file_test
-  #   compress gz
-  #   utc
-  # ]
 
-  def create_driver(conf = CONFIG, tag='test')
-    Fluent::Test::BufferedOutputTestDriver.new(Fluent::DataCounterOutput, tag).configure(conf)
+  def create_driver(conf = CONFIG, tag='test.input')
+    Fluent::Test::OutputTestDriver.new(Fluent::DataCounterOutput, tag).configure(conf)
   end
 
   def test_configure
-    #### set configurations
-    # d = create_driver %[
-    #   path test_path
-    #   compress gz
-    # ]
-    #### check configurations
-    # assert_equal 'test_path', d.instance.path
-    # assert_equal :gz, d.instance.compress
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver('')
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+        count_key field
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+        pattern1 hoge ^1\\d\\d$
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+        count_key field
+        pattern2 hoge ^1\\d\\d$
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+        count_key field
+        pattern1 hoge ^1\\d\\d$
+        pattern4 pos  ^4\\d\\d$
+      ]
+    }
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+        count_key field
+        pattern1 hoge ^1\\d\\d$
+        pattern2 hoge ^4\\d\\d$
+      ]
+    }
+    d = create_driver %[
+      count_key field
+      pattern1 ok ^2\\d\\d$
+    ]
+    assert_equal :minute, d.instance.unit
+    assert_equal :tag, d.instance.aggregate
+    assert_equal 'datacount', d.instance.tag
+    assert_nil d.instance.input_tag_remove_prefix
+    assert_equal 'field', d.instance.count_key
+    assert_equal 'ok ^2\d\d$', d.instance.pattern1
   end
 
-  def test_format
+  def test_count_initialized
+    d = create_driver %[
+      aggregate all
+      count_key field
+      pattern1 hoge 1\d\d
+      pattern2 moge 2\d\d
+    ]
+    assert_equal [0,0,0], d.instance.counts['all']
+  end
+
+  def test_countups
     d = create_driver
+    assert_nil d.instance.counts['test.input']
 
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
-
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":1}\n]
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":2}\n]
-
-    # d.run
+    d.instance.countups('test.input', [0, 0, 0, 0, 0])
+    assert_equal [0,0,0,0,0], d.instance.counts['test.input']
+    d.instance.countups('test.input', [1, 1, 1, 0, 0])
+    assert_equal [1,1,1,0,0], d.instance.counts['test.input']
+    d.instance.countups('test.input', [0, 5, 1, 0, 0])
+    assert_equal [1,6,2,0,0], d.instance.counts['test.input']
   end
 
-  def test_write
+  def test_stripped_tag
     d = create_driver
-
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
-
-    # ### FileOutput#write returns path
-    # path = d.run
-    # expect_path = "#{TMP_DIR}/out_file_test._0.log.gz"
-    # assert_equal expect_path, path
+    assert_equal 'input', d.instance.stripped_tag('test.input')
+    assert_equal 'test.input', d.instance.stripped_tag('test.test.input')
+    assert_equal 'input', d.instance.stripped_tag('input')
   end
+
+  def test_generate_output
+    d = create_driver
+    r1 = d.instance.generate_output({'test.input' => [60,240,120,180,0], 'test.input2' => [0,600,0,0,0]}, 60)
+    assert_equal   60, r1['input_unmatched_count']
+    assert_equal  1.0, r1['input_unmatched_rate']
+    assert_equal 10.0, r1['input_unmatched_percentage']
+    assert_equal  240, r1['input_status2xx_count']
+    assert_equal  4.0, r1['input_status2xx_rate']
+    assert_equal 40.0, r1['input_status2xx_percentage']
+    assert_equal  120, r1['input_status3xx_count']
+    assert_equal  2.0, r1['input_status3xx_rate']
+    assert_equal 20.0, r1['input_status3xx_percentage']
+    assert_equal  180, r1['input_status4xx_count']
+    assert_equal  3.0, r1['input_status4xx_rate']
+    assert_equal 30.0, r1['input_status4xx_percentage']
+    assert_equal    0, r1['input_status5xx_count']
+    assert_equal  0.0, r1['input_status5xx_rate']
+    assert_equal  0.0, r1['input_status5xx_percentage']
+
+    assert_equal    0, r1['input2_unmatched_count']
+    assert_equal  0.0, r1['input2_unmatched_rate']
+    assert_equal  0.0, r1['input2_unmatched_percentage']
+    assert_equal  600, r1['input2_status2xx_count']
+    assert_equal 10.0, r1['input2_status2xx_rate']
+    assert_equal 100.0, r1['input2_status2xx_percentage']
+    assert_equal    0, r1['input2_status3xx_count']
+    assert_equal  0.0, r1['input2_status3xx_rate']
+    assert_equal  0.0, r1['input2_status3xx_percentage']
+    assert_equal    0, r1['input2_status4xx_count']
+    assert_equal  0.0, r1['input2_status4xx_rate']
+    assert_equal  0.0, r1['input2_status4xx_percentage']
+    assert_equal    0, r1['input2_status5xx_count']
+    assert_equal  0.0, r1['input2_status5xx_rate']
+    assert_equal  0.0, r1['input2_status5xx_percentage']
+
+    d = create_driver %[
+      aggregate all
+      count_key field
+      pattern1 hoge xxx\d\d
+    ]
+    r2 = d.instance.generate_output({'all' => [60,240]}, 60)
+    assert_equal   60, r2['unmatched_count']
+    assert_equal  1.0, r2['unmatched_rate']
+    assert_equal 20.0, r2['unmatched_percentage']
+    assert_equal  240, r2['hoge_count']
+    assert_equal  4.0, r2['hoge_rate']
+    assert_equal 80.0, r2['hoge_percentage']
+  end
+
+  def test_emit
+    d1 = create_driver(CONFIG, 'test.tag1')
+    d1.run do
+      60.times do
+        d1.emit({'target' => '200'})
+        d1.emit({'target' => '100'})
+        d1.emit({'target' => '200'})
+        d1.emit({'target' => '400'})
+      end
+    end
+    r1 = d1.instance.flush(60)
+    assert_equal 120, r1['tag1_status2xx_count']
+    assert_equal 2.0, r1['tag1_status2xx_rate']
+    assert_equal 50.0, r1['tag1_status2xx_percentage']
+
+    assert_equal 60, r1['tag1_status4xx_count']
+    assert_equal 1.0, r1['tag1_status4xx_rate']
+    assert_equal 25.0, r1['tag1_status4xx_percentage']
+    
+    assert_equal 60, r1['tag1_unmatched_count']
+    assert_equal 1.0, r1['tag1_unmatched_rate']
+    assert_equal 25.0, r1['tag1_unmatched_percentage']
+    
+    assert_equal 0, r1['tag1_status3xx_count']
+    assert_equal 0.0, r1['tag1_status3xx_rate']
+    assert_equal 0.0, r1['tag1_status3xx_percentage']
+    assert_equal 0, r1['tag1_status5xx_count']
+    assert_equal 0.0, r1['tag1_status5xx_rate']
+    assert_equal 0.0, r1['tag1_status5xx_percentage']
+
+    d2 = create_driver(%[
+      aggregate all
+      count_key target
+      pattern1 ok 2\\d\\d
+      pattern2 redirect 3\\d\\d
+    ], 'test.tag2')
+    d2.run do
+      60.times do
+        d2.emit({'target' => '200'})
+        d2.emit({'target' => '300 200'})
+      end
+    end
+    d2.instance.flush_emit(120)
+    emits = d2.emits
+    assert_equal 1, emits.length
+    data = emits[0]
+    assert_equal 'datacount', data[0] # tag
+    assert_equal 120, data[2]['ok_count']
+    assert_equal 1.0, data[2]['ok_rate']
+    assert_equal 100.0, data[2]['ok_percentage']
+    assert_equal 0, data[2]['redirect_count']
+    assert_equal 0.0, data[2]['redirect_rate']
+    assert_equal 0.0, data[2]['redirect_percentage']
+    assert_equal 0, data[2]['unmatched_count']
+    assert_equal 0.0, data[2]['unmatched_rate']
+    assert_equal 0.0, data[2]['unmatched_percentage']
+  end
+
 end
