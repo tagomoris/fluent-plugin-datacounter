45da908aa32af94e7ce997fb517a4db28dc75c6f
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-02-22 17:20:26 +0900

    fix to supress fluentd logs on test

--- test/helper.rb
+++ test/helper.rb
@@ -12,6 +12,16 @@ require 'test/unit'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'fluent/test'
+unless ENV.has_key?('VERBOSE')
+  nulllogger = Object.new
+  nulllogger.instance_eval {|obj|
+    def method_missing(method, *args)
+      # pass
+    end
+  }
+  $log = nulllogger
+end
+
 require 'fluent/plugin/out_datacounter'
 
 class Test::Unit::TestCase
